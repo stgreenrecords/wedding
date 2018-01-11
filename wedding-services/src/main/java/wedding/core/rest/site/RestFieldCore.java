9e4663f342f7e451ce0f390689eccdcba880d3cc
Viachaslau_Karnausha <stgreenrecords@gmail.com>
2018-01-11 16:26:57 +0300

    add MainRestServlet

--- wedding-services/src/main/java/wedding/core/rest/site/RestFieldCore.java
+++ wedding-services/src/main/java/wedding/core/rest/site/RestFieldCore.java
@@ -4,7 +4,7 @@ import org.apache.sling.api.SlingHttpServletRequest;
 
 public interface RestFieldCore {
 
-    String PROPERTY_CATALOG_TITLE = "catalogTitle";
+    String PROPERTY_CATALOG_TITLE = "jcr:title";
 
     Object apply(SlingHttpServletRequest request);
 
