758bc5e5d1ce594519e9fc0e546cb3db780f8f5a
Viachaslau_Karnausha <stgreenrecords@gmail.com>
2018-01-11 15:51:16 +0300

    add MainRestServlet

--- wedding-services/src/main/java/wedding/core/rest/site/CatalogCategories.java
+++ wedding-services/src/main/java/wedding/core/rest/site/CatalogCategories.java
@@ -17,7 +17,7 @@ import static java.util.stream.Collectors.toMap;
 public class CatalogCategories implements RestFieldCore {
 
     @Override
-    public Object apply(SlingHttpServletRequest request) {
+    public Map<String, String> apply(SlingHttpServletRequest request) {
         return Optional.ofNullable(request.getResourceResolver().getResource(Constants.CATALOG_ROOT_PAGE_PATH))
                 .map(Resource::listChildren)
                 .map(WeddingResourceUtil::iteratorToOrderedStream)
