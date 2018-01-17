a08e70f9fbe056dba916771518fb4ed9876ae569
Viachaslau_Karnausha <stgreenrecords@gmail.com>
2018-01-17 14:54:37 +0300

    add filter

--- wedding-services/src/main/java/wedding/core/rest/site/Partners.java
+++ wedding-services/src/main/java/wedding/core/rest/site/Partners.java
@@ -1,5 +1,6 @@
 package wedding.core.rest.site;
 
+import org.apache.commons.lang3.StringUtils;
 import org.apache.felix.scr.annotations.Component;
 import org.apache.felix.scr.annotations.Service;
 import org.apache.sling.api.SlingHttpServletRequest;
@@ -7,7 +8,10 @@ import org.apache.sling.api.resource.Resource;
 import wedding.core.model.UserData;
 import wedding.core.utils.WeddingResourceUtil;
 
+import java.util.Arrays;
 import java.util.Optional;
+import java.util.function.BiFunction;
+import java.util.function.Predicate;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
@@ -17,15 +21,51 @@ public class Partners implements RestFieldCore {
 
     @Override
     public Object apply(SlingHttpServletRequest request) {
-        String city = request.getParameter(REQUEST_PARAMETER_CITY);
-        return Optional.ofNullable(request.getParameter(REQUEST_PARAMETER_CATEGORY_PATH))
-                .map(path -> request.getResourceResolver().getResource(path))
-                .map(categoryResource -> categoryResource.getChild(city))
+        String[] selectors = request.getRequestPathInfo().getSelectors();
+        String speciality = Arrays.stream(selectors).skip(1).findFirst().orElse(StringUtils.EMPTY);
+        String city = Arrays.stream(selectors).skip(2).findFirst().orElse(StringUtils.EMPTY);
+        return Optional.ofNullable(request.getResourceResolver().getResource(PARTNER_USERS_ROOT_PATH))
+                .map(partnerRoot -> partnerRoot.getChild(speciality))
+                .map(specialityResource -> specialityResource.getChild(city))
                 .map(Resource::listChildren)
                 .map(WeddingResourceUtil::iteratorToOrderedStream)
                 .orElse(Stream.empty())
                 .map(resource -> resource.adaptTo(UserData.class))
+                .filter(applyFilters(request))
                 .collect(Collectors.toList());
+    }
+
+    private Predicate<UserData> applyFilters(final SlingHttpServletRequest request) {
+        return userData -> Arrays.stream(Filters.values()).
+                anyMatch(filter -> filter.applyFilter(userData, request));
+    }
+
+    private enum Filters{
+
+        FILTER_BY_PRICE_START(((userData, request) -> {
+            return Optional.ofNullable(request.getParameter(REQUEST_PARAMETER_PRICE_START))
+                    .map(parameterValue -> Integer.parseInt(userData.getPriceStart()) < Integer.parseInt(parameterValue))
+                    .orElse(true);
+        })),
+
+        FILTER_BY_PRICE_END(((userData, request) -> {
+            return Optional.ofNullable(request.getParameter(REQUEST_PARAMETER_PRICE_END))
+                    .map(parameterValue -> Integer.parseInt(userData.getPriceEnd()) < Integer.parseInt(parameterValue))
+                    .orElse(true);
+        }));
+
+        private String requestParameter;
+        private BiFunction<UserData, SlingHttpServletRequest, Boolean> function;
+
+        Filters(final BiFunction<UserData, SlingHttpServletRequest, Boolean> function) {
+            this.requestParameter = requestParameter;
+            this.function = function;
+        }
+
+        public boolean applyFilter(UserData userData, SlingHttpServletRequest request){
+            return function.apply(userData, request);
+        }
+
     }
 
 
--- wedding-services/src/main/java/wedding/core/rest/site/RestFieldCore.java
+++ wedding-services/src/main/java/wedding/core/rest/site/RestFieldCore.java
@@ -9,7 +9,13 @@ public interface RestFieldCore {
     String PROPERTY_CATALOG_TITLE = "jcr:title";
 
     String REQUEST_PARAMETER_CATEGORY_PATH = "categoryPath";
-    String REQUEST_PARAMETER_CITY = "city";
+    String REQUEST_PARAMETER_SORTED_BY = "sortBy";
+    String REQUEST_PARAMETER_PRICE_DOWN = "priceDown";
+    String REQUEST_PARAMETER_PRICE_START = "priceStart";
+    String REQUEST_PARAMETER_PRICE_END = "priceEnd";
+    String REQUEST_PARAMETER_DATE = "date";
+
+    String PARTNER_USERS_ROOT_PATH = "/home/users/wedding/partners";
 
     Object apply(SlingHttpServletRequest request);
 
