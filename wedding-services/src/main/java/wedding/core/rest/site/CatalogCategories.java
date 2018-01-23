ace84d777c9bcd4eb643139b7bb577cdb44701c0
Viachaslau_Karnausha <stgreenrecords@gmail.com>
2018-01-23 11:41:33 +0300

    add tender

--- wedding-services/src/main/java/wedding/core/rest/site/CatalogCategories.java
+++ wedding-services/src/main/java/wedding/core/rest/site/CatalogCategories.java
@@ -4,10 +4,10 @@ import org.apache.felix.scr.annotations.Component;
 import org.apache.felix.scr.annotations.Service;
 import org.apache.sling.api.SlingHttpServletRequest;
 import org.apache.sling.api.resource.Resource;
-import wedding.core.data.Constants;
 import wedding.core.utils.WeddingResourceUtil;
 
-import java.util.*;
+import java.util.Map;
+import java.util.Optional;
 import java.util.stream.Stream;
 
 import static java.util.stream.Collectors.toMap;
@@ -19,7 +19,7 @@ public class CatalogCategories implements RestFieldCore {
     @Override
     public Map<String, String> apply(SlingHttpServletRequest request) {
         return Optional.of(request.getResourceResolver())
-                .map(resolver -> resolver.getResource(Constants.CATALOG_ROOT_PAGE_PATH))
+                .map(resolver -> resolver.getResource(PARTNER_USERS_ROOT_PATH))
                 .map(Resource::listChildren)
                 .map(WeddingResourceUtil::iteratorToOrderedStream)
                 .orElse(Stream.empty())
