db30c6e63c12e8a4e865e28645ab478f562f6771
@Anthony-Hoopkins <aantonij@gmail.com>
2018-03-30 19:33:51 +0300

    dop

--- wedding-view/src/main/content/jcr_root/apps/wedding/components/pages/category/js/script.js
+++ wedding-view/src/main/content/jcr_root/apps/wedding/components/pages/category/js/script.js
@@ -11,7 +11,7 @@ var PORTAL = (function (PORTAL, $) {
         (function () { // ============   Запросы
 
             $.ajax({
-                url: "http://wedding-services.mycloud.by/services/rest.partners/photographers/minsk.12.json",
+                url: "http://wedding-services.mycloud.by/services/rest.partners/photographers/minsk.json",
                 type: "GET",
                 dataType: "json",
                 success: function (data) {
@@ -19,6 +19,7 @@ var PORTAL = (function (PORTAL, $) {
                     console.log("success");
 
                     console.dir(data);
+
                     console.dir(photo_first_five);
                     console.log(photo_first_five[4].firstName);
                     console.log(photo_first_five[4].lastName);
@@ -37,38 +38,20 @@ var PORTAL = (function (PORTAL, $) {
                     pro_div2.querySelector(".pro_vk").innerHTML = photo_first_five[1].vkLink;
 
                     var first_div = document.querySelector(".search-usually");
-                    var main_container = document.querySelector("#search-usually-cont");
-
-                    for (let i = 0; i < photo_first_five.length; i++) {
-                        let copy_div = first_div.cloneNode(true);
-                        copy_div.querySelector(".search-usually_img").style.backgroundImage = "url('/etc/clientlibs/wedding/pages/images/any_img/d2_" + (i + 1) + ".jpg')";
-                        copy_div.querySelector(".search-usually_content_name").innerHTML = photo_first_five[i].firstName + " " + photo_first_five[i].lastName;
+                    var main_container =  document.querySelector("#search-usually-cont");
+                    var k=0;
+                    for (var i = 0; i<photo_first_five.length; i++){
+                        var copy_div = first_div.cloneNode(true);
+                        k= i%9+1;
+                        copy_div.querySelector(".search-usually_img").style.backgroundImage = "url('/etc/clientlibs/wedding/pages/images/any_img/d2_"+k+".jpg')";
+                        copy_div.querySelector(".search-usually_content_name").innerHTML = photo_first_five[i].firstName + " "+ photo_first_five[i].lastName;
                         copy_div.querySelector(".search-usually_content_speciality").innerHTML = photo_first_five[i].speciality;
                         main_container.appendChild(copy_div);
                     }
+
                 }  //  success finish
+
             });  // ---  AJAX finish
-
-            // - ==========================================       ЗАкомментировано до возвращения к верстке на КАТАЛОГА 13.03.2018
-      /*      $.ajax({  // ---  AJAX 2
-                url: "https://api.github.com/emojis",
-                type: "GET",
-                dataType: "json",
-                success: function (data) {
-                    var xc = data;
-                    console.log("success");
-                    console.dir(data);
-                    var second_div = document.querySelector(".search-usually");
-                    var main_container2 = document.querySelector("#search-usually-cont");
-
-                    for (let prop in xc) {
-                        let copy_div = second_div.cloneNode(true);
-                        copy_div.querySelector(".search-usually_img").style.backgroundImage = "url('" + xc[prop] + "')";
-                        main_container2.appendChild(copy_div);
-                    }
-
-                }
-            });             // ---  AJAX 2 finish
 
             setTimeout(function () {
                 $.ajax({  // ---  AJAX All
@@ -76,18 +59,31 @@ var PORTAL = (function (PORTAL, $) {
                     type: "GET",
                     dataType: "json",
                     success: function (data) {
-                        var photo_all = data;
+                        var allUser = data;
                         var all_div = document.querySelectorAll(".search-usually");
-
-                        for (var i = 0; i < all_div.length; i++) {
-                            all_div[i].querySelector(".search-usually_content_name").innerHTML = photo_all[i].firstName + " " + photo_all[i].lastName;
-                            all_div[i].querySelector(".search-usually_content_speciality").innerHTML = photo_all[i].speciality;
+                        var first_div = document.querySelector(".search-usually");
+                        var main_container =  document.querySelector("#search-usually-cont");
+                        var k=0;
+                        for (var i = 0; i<allUser.length; i++){
+                            var copy_div = first_div.cloneNode(true);
+                            k= i%9+1;
+                            copy_div.querySelector(".search-usually_img").style.backgroundImage = "url('/etc/clientlibs/wedding/pages/images/any_img/d2_"+k+".jpg')";
+                            copy_div.querySelector(".search-usually_content_name").innerHTML = allUser[i].firstName + " "+ allUser[i].lastName;
+                            copy_div.querySelector(".search-usually_content_speciality").innerHTML = allUser[i].speciality;
+                            main_container.appendChild(copy_div);
                         }
-
                     }
                 })
-            }, 1000);  */   // ---  AJAX All finish
+            }, 1000);     // ---  AJAX All finish
 
+            $.ajax({
+                url: "http://wedding-services.mycloud.by/services/rest.users/minsk.json?userId=a931a267-ff17-4d89-ab80-e478c0a6de0a",
+                type: "GET",
+                dataType: "json",
+                success: function (data) {
+                    console.dir(data);
+                }
+            });
 
         }()); // end -  --- Запросы
 
