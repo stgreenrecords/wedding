01cb9c88e04bccc8b39a87233dfed9cf4b207b3c
@Anthony-Hoopkins <aantonij@gmail.com>
2018-06-12 15:22:19 +0300

    settings

--- wedding-view/src/main/content/jcr_root/apps/wedding/components/pages/settings/js/script.js
+++ wedding-view/src/main/content/jcr_root/apps/wedding/components/pages/settings/js/script.js
@@ -8,7 +8,31 @@ var PORTAL = (function (PORTAL, $) {
 
         console.log('Component: "Settings"');
 
-    };
+        (function(){  //  функции переключения страниц
+            var user_calc  = $self.find('#settings-common');
+            var user_tend  = $self.find('#settings-notification');
+            var calc_btn =  $self.find('#btn_settings-common');
+            var tend_btn = $self.find('#btn_settings-notif');
+
+            calc_btn.on('click', openPageAllSett);
+            tend_btn.on('click', openPageNotificationSett);
+
+            function openPageAllSett(event){
+                user_tend.addClass('hidden_full');
+                user_calc.removeClass('hidden_full');
+                tend_btn.removeClass('user_menu_btn_active');
+                calc_btn.addClass('user_menu_btn_active');
+            }
+
+            function openPageNotificationSett(event){
+                user_calc.addClass('hidden_full');
+                user_tend.removeClass('hidden_full');
+                calc_btn.removeClass('user_menu_btn_active');
+                tend_btn.addClass('user_menu_btn_active');
+            }
+        }());
+
+    };  //----
 
     return PORTAL;
 
