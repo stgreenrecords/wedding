0964b77ef589eb972c892fa0e0dc36b744f2552e
Viachaslau Karnaushanka[EPAM] <viachaslua_karnaushanka@epam.com>
2018-11-01 16:35:36 +0300

    rest

--- wedding-services/src/main/java/wedding/core/rest/site/Partners.java
+++ wedding-services/src/main/java/wedding/core/rest/site/Partners.java
@@ -6,10 +6,15 @@ import org.apache.felix.scr.annotations.Property;
 import org.apache.felix.scr.annotations.Service;
 import org.apache.sling.api.SlingHttpServletRequest;
 import wedding.core.model.PartnerModel;
+import wedding.core.utils.WeddingResourceUtil;
 
+import javax.jcr.query.Query;
 import java.util.Arrays;
 import java.util.Comparator;
+import java.util.Objects;
 import java.util.Optional;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 @Component(immediate = true)
 @Service(Partners.class)
@@ -22,10 +27,16 @@ import java.util.Optional;
 })
 public class Partners extends AbstractResFieldCore {
 
-    private Comparator<PartnerModel> applySorting(final SlingHttpServletRequest request) {
-        return Optional.ofNullable(request.getParameter(REQUEST_PARAMETER_SORTED_BY))
-                .map(Comparators::getComparatorByName)
-                .orElse(Comparators.COMPARATOR_BY_VIP_STATUS.getComparator());
+    @Override
+    public Object getObject(SlingHttpServletRequest request) {
+        return Optional.of(request.getResourceResolver())
+                .map(resolver -> resolver.findResources(String.format(PARTNER_QUERY, WeddingResourceUtil.getSuffixPathFromRequest(request), WeddingResourceUtil.getIdQueryPart(request)), Query.SQL))
+                .map(WeddingResourceUtil::iteratorToOrderedStream)
+                .orElse(Stream.empty())
+                .map(resource -> resource.adaptTo(PartnerModel.class))
+                .filter(Objects::nonNull)
+                .limit(getLimit(request))
+                .collect(Collectors.toList());
     }
 
     private enum Comparators {
